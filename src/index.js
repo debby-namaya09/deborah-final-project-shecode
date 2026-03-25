460eb6c84c9d59c41a09488ac69b5a30569e7a9d
debby-namaya09 <153802138+debby-namaya09@users.noreply.github.com>
2026-03-25 21:05:43 +0200

    added an api

--- src/index.js
+++ src/index.js
@@ -79,11 +79,11 @@ function formatDate(date) {
 
 function searchCity(city) {
   let apiKey = "b2a5adcct04b33178913oc335f405433";
-  let apiUrl = `https://api.shecodes.io/weather/v1/current?query=${city}&key=${apiKey}&units=metric`;
+  let url = `https://api.shecodes.io/weather/v1/forecast?query=${city}&key=${apiKey}&units=metric`;
 
   axios.get(apiUrl).then(function (response) {
     refreshWeather(response);
-    getForecast(response.data.city); // ✅ THIS CALLS FORECAST
+    getForecast(response.data.city); 
   });
 }
 
